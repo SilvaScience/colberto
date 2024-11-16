e69d5ec00b48952aa477d9ce645b673418733a37
Félix Thouin <felix.thouin@umontreal.ca>
2024-11-15 19:35:28 -0500

    Would need to make driver async too. Not ideal

--- samples/engine/simple_gui_demo.py
+++ samples/engine/simple_gui_demo.py
@@ -1,17 +1,30 @@
 import asyncio
 import time
+from dumSpec import dumSpec1000
 
 async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
 
+#async def say_after(delay, what):
+#    await asyncio.sleep(delay)
+#    print(what)
+
 async def main():
-    print(f"started at {time.strftime('%X')}")
+    spec=dumSpec1000(integration_time=2)
+    loop = asyncio.get_running_loop()
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(
+            say_after(1, 'hello'))
 
-    await say_after(1, 'hello')
-    await say_after(2, 'world')
+        task2 = tg.create_task(
+            say_after(2, 'world'))
 
-    print(f"finished at {time.strftime('%X')}")
+        print(f"started at {time.strftime('%X')}")
 
-asyncio.run(main())
+    # The await is implicit when the context manager exits.
+
+    print(f"finished at {time.strftime('%X')}")#
+if __name__=='__main__':
+    asyncio.run(main())
 
