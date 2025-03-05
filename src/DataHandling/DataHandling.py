5f87e92d2fdedb48fc2ff2da96c691f96f615053
Félix Thouin <felix.thouin@umontreal.ca>
2025-03-05 11:42:06 -0500

    Agreed on a beam handling scheme with SImon (OP513 and OP514 OP557)

--- src/DataHandling/DataHandling.py
+++ src/DataHandling/DataHandling.py
@@ -25,6 +25,7 @@ class DataHandling(QtCore.QThread):
     sendSpectrum = QtCore.pyqtSignal(np.ndarray, np.ndarray)
     sendMaximum = QtCore.pyqtSignal(np.ndarray) # not used for now, to be implemented for direct measurment control
     sendParameterarray = QtCore.pyqtSignal(np.ndarray, np.ndarray)
+    sendBeams = QtCore.pyqtSignal(object)
 
     def __init__(self, parameter, speclength):
         super(DataHandling, self).__init__()
@@ -57,6 +58,8 @@ class DataHandling(QtCore.QThread):
 
         # initialize Calibration dict
         self.calibration = {}
+        # initialize beams dict
+        self.beams={}
 
     # main update device parameter function
     def update_parameter(self, parameter):
@@ -168,6 +171,22 @@ class DataHandling(QtCore.QThread):
         # to be used from calibration scripts. Each calibration should consist of a tuple of name and content
         calibration_name, calibration_value = calibration
         self.calibration[calibration_name] = calibration_value
+    
+    def set_beam(self, name_and_beam):
+        '''
+            Adds or updates the beam at the provided index
+            input:
+                - tuple: (name,Beam) First argument of tuple is beam name and second is Beam object to set
+        '''
+        beam_name,beam=name_and_beam
+        self.beams[beam_name]=beam
+
+    def get_beams(self):
+        '''
+            Triggers emission of beams to connected slots when called
+        '''
+        self.sendBeams.emit(self.beams)
+
 
     def add_attribute(self,attribute):
         # to be used from measurment each attribute should consist of a tuple of name and content
