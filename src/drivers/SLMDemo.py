7594a3643061e142b53d73feaa270b46419dc7c6
Katie <kochka22@wfu.edu>
2025-04-02 15:11:20 -0400

    Update SLMDemo.py

--- src/drivers/SLMDemo.py
+++ src/drivers/SLMDemo.py
@@ -36,9 +36,15 @@ class SLMDemo(QtWidgets.QMainWindow):
         self.parameter_display_dict['amplitude']['unit'] = ' V'
         self.parameter_display_dict['amplitude']['max'] = 1000
         self.parameter_display_dict['amplitude']['read'] = False
+        self.parameter_display_dict['greyscale_val']['val'] = 0
+        self.parameter_display_dict['greyscale_val']['unit'] = ' '
+        self.parameter_display_dict['greyscale_val']['max'] = 255
+        self.parameter_display_dict['greyscale_val']['read'] = False
 
         # set parameters
         self.amplitude = 5
+        self.temperature = 300
+        self.greyscale_val = 0
 
         # set up parameter dict that only contains value. (faster to access)
         self.parameter_dict = {}
@@ -49,10 +55,29 @@ class SLMDemo(QtWidgets.QMainWindow):
         project_folder = Path(__file__).parents[1].resolve()
         uic.loadUi(Path(project_folder,r'GUI/SLM_GUI.ui'), self)
 
-    def set_parameter(self, parameter, value):
+        def set_parameter(self, parameter, value):
         """REQUIRED. This function defines how changes in the parameter tree are handled.
         In devices with workers, a pause of continuous acquisition might be required. """
         if parameter == 'amplitude':
             self.parameter_dict['amplitude'] = value
             self.amplitude = value
+        elif parameter == 'temperature':
+            self.parameter_dict['temperature'] = value
+            self.temperature = value
+        elif parameter == 'greyscale_val':
+            self.parameter_dict['greyscale_val'] = value
+            self.greyscale_val = value
+
+    def parameter_slm(self):
+        rgb=1
+        bit=1
+        height= 1200
+        width = 8
+        depth = 1920
+        RGB   = ctypes.c_uint(rgb)
+        isEightBitImage = ctypes.c_uint(bit)
+        return height,width,depth,RGB,isEightBitImage
+
+    def write_image(self, image):
+        return 42
 
