db0a4d2f8c44240442020c7c578748fe81157f05
Katie <kochka22@wfu.edu>
2025-04-22 16:04:49 -0400

    Add files via upload

---
+++ src/GUI/LUT_Calib_plot.py
@@ -0,0 +1,43 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+import pyqtgraph as pg
+import numpy as np
+import time
+
+class LUT_Calib_plot(QtWidgets.QMainWindow):
+
+
+    def __init__(self, plotWidget, *args, **kwargs):
+        '''
+            Initializes the graph to diplay the spectra taken during a LUT calibration.
+        '''
+        super(LUT_Calib_plot, self).__init__(*args, **kwargs)
+
+        # create Widgets for plot
+        self.graphWidget = plotWidget
+        self.styles = {'color':'#c8c8c8', 'font-size':'20px'}
+        self.fontForTickValues = QtGui.QFont()
+        self.fontForTickValues.setPixelSize(10)
+        self.ydata=None
+        self.xdata=None
+        self.regions=None
+
+        # plot data: x, y values
+        self.graphWidget.getAxis('left').setStyle(tickFont=self.fontForTickValues)
+        self.graphWidget.getAxis('bottom').setStyle(tickFont=self.fontForTickValues)
+        self.graphWidget.setLabel('left', 'Intensity', **self.styles)
+        self.graphWidget.setLabel('bottom', 'Wavelength (nm)', **self.styles)
+        self.graphWidget.showGrid(True, True)
+        # Clear data to show plot
+        self.clear_plot()
+
+
+    @QtCore.pyqtSlot()
+    def clear_plot(self):
+        self.graphWidget.clear()
+
+    @QtCore.pyqtSlot(np.ndarray, np.ndarray)
+    def set_data(self, x_array, y_array):
+        self.xdata=x_array
+        self.ydata=y_array
+        self.graphWidget.clear()
+        self.graphWidget.plot(x_array,y_array)
