020531ce54068504ae598fc31280ed668955a49a
Simon Daneau <simon.daneau@umontreal.ca>
2025-11-04 14:49:47 -0700

    Update Instruments.py

--- src/drivers/Instruments.py
+++ src/drivers/Instruments.py
@@ -61,7 +61,7 @@ def load_instruments():
     stresing_params={
         'pixel_size_mm':24e-3,
         'num_pixels':1010,
-        'calibrated': True,
+        'calibrated': False,
         'calibrationSlope': 0.955,
         'calibrationOffset': -30.864
     }
