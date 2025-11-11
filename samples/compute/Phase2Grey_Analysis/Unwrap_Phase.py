f0d0b7ceee7ef3f92cb361cd556bf764eb135a00
Katie <kochka22@wfu.edu>
2025-11-11 13:19:00 -0500

    Update Unwrap_Phase.py

--- samples/compute/Phase2Grey_Analysis/Unwrap_Phase.py
+++ samples/compute/Phase2Grey_Analysis/Unwrap_Phase.py
@@ -1,184 +1,217 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul  3 15:44:01 2025
+Created on Thu Oct 23 15:30:59 2025
 
 @author: katiekoch
 """
+
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
-from scipy.signal import savgol_filter, argrelextrema
+from scipy.signal import savgol_filter, argrelextrema, find_peaks
+import Functions as kak
 
-fn = '06052025_WL_Test4_13_26_32.h5'
+############  '''## Input file path ##''' #############
+# 06052025_WL_Test4_13_26_32.h5
 
-with h5py.File(fn,'r') as hdf:
-    
-    ls = list(hdf.keys())
-    print('List of Data Sets in this file: \n', ls)
-    
-    data = hdf.get('spectra')
-    data_set = np.array(data)
-    #print(data_set.shape)
-    
-    param_set = hdf.get('parameter')
-    param_set = np.array(param_set)
-    
-    data = hdf.get('spectra')
-    
-    grp = hdf['spectra']
-    
-    grf = hdf['parameter']
+# 2025_11_03_Spectra_SLM_Calib_2scan_FYLA_450nm_750nm_14_22_28.h5
 
-    #print(grp.attrs['parameter_keys'])
-    params = grf.attrs['parameter_keys']
-    wave = grp.attrs['xaxis']
-###############################################################################
-try:
-    #index = params.index('greyscale_val')
-    index = np.where(params=='greyscale_val')
-    index = np.array(index)
-    idx_val = int(index[0])
-    print("String found at index", idx_val)
-except ValueError:
-    print("String not found!")
-###############################################################################
-Total_GreyScale_Vals = param_set[idx_val,:]
+fn = r"Data_Files/Nov_Data/2025_11_03_Spectra_SLM_Calib_2scan_FYLA_450nm_750nm_14_22_28.h5"
+
+############  '''## Load Data & Process ##''' #############
+
+[wave,spectra,Total_GreyScale_Vals] = kak.load_data(fn)
+
 GreyScale_Vals = np.unique(Total_GreyScale_Vals)
+############  '''## Average Spectra from Multiple Scans ##''' #############
+avg_spectrum = np.zeros(
+    (len(wave), len(GreyScale_Vals)))  # set array size to average spectrum from diff scans
+for j in range(len(GreyScale_Vals)):
+    for i in range(len(Total_GreyScale_Vals)):
+        if Total_GreyScale_Vals[i] == GreyScale_Vals[j]:
+            avg_spectrum[:, j] = avg_spectrum[:, j] + spectra[:, i]
 
-cut = len(params)
-print(cut)    
+scan_num = len(Total_GreyScale_Vals) / len(GreyScale_Vals)
+print('scan_num', scan_num)
 
-spectra = data_set
-###############################################################################
-plt.plot(wave,spectra[:,1:])
-plt.xlim([450,1100])
+avg_spectrum = avg_spectrum/scan_num
+
+# avg_spectrum = spectra[:,1:] # only for Montreal Data_Set
+# GreyScale_Vals = Total_GreyScale_Vals[1:]
+
+############  '''## Plot: wavelength spectrum for all grayscale values ##''' #############
+wave_xlim = [420, 760]
+
+plt.figure()
+plt.title("wavelengh spectrum for all greyscale value")  # (kept as in original)
+plt.plot(wave, avg_spectrum)
+plt.xlim([wave_xlim[0], wave_xlim[1]])
 plt.xlabel('Wavelength (nm)')
 plt.ylabel('Intensity (arb. u.)')
 plt.show()
 
-avg_spectrum = spectra[:,1:]
-GreyScale_Vals = Total_GreyScale_Vals[1:]
-###############################################################################
+############  '''## Trim Spectra to Relevent Wavelength Range ##''' #############
+wavelength_cutoff_1 = 250
+print('#################################################################')
+print('Low Wavelength Cutoff', wave[wavelength_cutoff_1])
+
+wavelength_cutoff_2 = 1100
+print('#################################################################')
+print('High Wavelength Cutoff', wave[wavelength_cutoff_2])
+
+trim_wave = wave[wavelength_cutoff_1:wavelength_cutoff_2]
+trim_avg_spectrum = avg_spectrum[wavelength_cutoff_1:wavelength_cutoff_2,:]
+
+# test plot
 plt.figure()
+plt.title("wavelengh spectrum for all greyscale value")  # (kept as in original)
+plt.plot(trim_wave, trim_avg_spectrum)
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Intensity (arb. u.)')
+plt.show()
 
-for i in range(len(wave)):
-
-    plt.plot(GreyScale_Vals,avg_spectrum[i,:])
-    
-    plt.xlabel('Wavelength (nm)')
+############  '''## Plot: grayscale scans for all relevant wavelengths ##''' #############
+plt.figure()
+plt.title("grayscale spectrum for all wavelength value")  # (kept as in original)
+for i in range(len(trim_wave)):
+    plt.plot(GreyScale_Vals, trim_avg_spectrum[i, :])
+    plt.xlabel('Grey value (0-255)')
     plt.ylabel('Intensity (arb. u.)')
-    
-plt.show()
-###############################################################################
-cut = 300 #300
-print(wave[cut])
-###############################################################################
-
-###############################################################################
-'''## Plot intensity as a function of grayscle for one wavelength ##'''
-###############################################################################
-plt.plot(GreyScale_Vals,avg_spectrum[cut,:], label = wave[cut])
-plt.xlabel('Grayscale Value')
-plt.ylabel('Intensity')
-plt.legend()
 plt.show()
 
-## trim grayscale options - only one period ##
+############  '''## Select wavelength index and grayscale trim ##''' #############
 
-idx1 = 1
-idx2 = 250
+cut = 140  # EN: wavelength selection (index in wave)
+print('#################################################################')
+print('Wavelegnth of cut', trim_wave[cut])
+print('#################################################################')
 
-# idx1 = 75
-# idx2 = 135
+idx1 = 0     # EN: trim grayscale options
+idx2 = 255   # FR: rognage des options de niveaux de gris
 
-plt.plot(GreyScale_Vals[idx1:idx2],avg_spectrum[cut,idx1:idx2], label = wave[cut])
-plt.xlabel('Grayscale Value')
-plt.ylabel('Intensity')
-plt.legend()
-plt.show()
-
-###############################################################################
-'''##     Normalize for ArcCos Range (-1, 1)    ##'''
-###############################################################################
+# Common grayscale window for all wavelengths
 g_vals = GreyScale_Vals[idx1:idx2]
-yData = avg_spectrum[cut,idx1:idx2]
+yData  = trim_avg_spectrum[cut, idx1:idx2]
 
-yData_remove_avg = yData - (np.min(yData)+np.max(yData))/2
-yData_Norm = yData_remove_avg*2/(np.max(yData_remove_avg)-np.min(yData_remove_avg))
-
-plt.plot(g_vals,yData_Norm)#, label = wave[cut])
+############  '''## Plot intensity vs grayscale (single wavelength) ##''' #############
+# EN: Optional: restrict to one period by trimming grayscale range
+# FR: Optionnel : restreindre à une période via le rognage des niveaux de gris
+plt.figure()
+plt.title("")
+plt.plot(GreyScale_Vals[idx1:idx2], trim_avg_spectrum[cut, idx1:idx2], label=trim_wave[cut])
 plt.xlabel('Grayscale Value')
 plt.ylabel('Intensity')
-
-plt.show()
-
-###############################################################################
-'''##     Compute Phase - ArcCos of Intensity Profile    ##'''
-###############################################################################
-phi_base = np.arccos(yData_Norm)
-
-phi_smooth = savgol_filter(phi_base, window_length=11, polyorder=3)
-
-plt.plot(g_vals,phi_base, label = 'phase')
-plt.plot(g_vals,phi_smooth, label = 'smoothed phase')
-plt.xlabel('Grayscale Value')
-plt.ylabel('Phase (rad.)')
 plt.legend()
 plt.show()
 
-# phase_unwrap = np.unwrap(phi_base)
-
-# plt.plot(g_vals,phi_base, label = 'phase')
-# plt.plot(g_vals,phase_unwrap, label = 'unwrap')
-# plt.xlabel('Grayscale Value')
-# plt.ylabel('Phase (rad.)')
-# plt.legend()
-# plt.show()
-
-###############################################################################
-'''##     Manually Unwrap Phase   ##'''
-###############################################################################
-dphi = np.gradient(phi_smooth)
-
-plt.plot(g_vals,dphi)
-plt.xlabel('Grayscale Value')
-plt.ylabel('Phase (rad.)')
-plt.show()
-
-threshold = 3  # define threshold for jump
-wrap_locs = np.where(dphi < threshold)[0] # array of positons where the phase jumps
-
-# Step 4: Manually unwrap phase
-unwrapped_phase = phi_base.copy()
-offset = 0 # keeps track of cmulative phase added due to unwrapping
-
-for i in range(1, len(unwrapped_phase)):
-    if i in wrap_locs:
-        offset += np.pi #adds pi to the offset 
-    unwrapped_phase[i] += offset # adds the current cumulative offset to the phase value at that point
-
-plt.plot(g_vals, phi_base, label='Measured Phase (wrapped)', alpha=0.7)
-plt.plot(g_vals, unwrapped_phase, label='Manually Unwrapped Phase', linewidth=2)
-plt.legend()
-plt.xlabel('Grayscale Value')
-plt.ylabel('Phase (rad)')
-plt.show()
-
-###############################################################################
-'''##     Fit to Fifth Order Polynomial   ##'''
-###############################################################################
-phase = unwrapped_phase
-
+############  '''## Fit 5th-order poly for ONE wavelength ##''' #############
 order = 5
-coeffs = np.polyfit(g_vals, phase, order)
-poly5 = np.poly1d(coeffs)
-phase_fit = poly5(g_vals)
+prom_frac=0.08
+dist_frac=1/4
+savgol_window=11
+savgol_polyorder=3
 
-plt.plot(g_vals, phase, 'ko', label='Manually Unwrapped Phase')
-plt.plot(g_vals, phase_fit, 'r-', label='5th-order polynomial fit')
-plt.xlabel('Grayscale Value')
-plt.ylabel('Phase (rad)')
-plt.legend()
-plt.show()
+yData  = trim_avg_spectrum[cut, idx1:idx2]
+
+#print("fit domain:", g_vals[idx1:idx2][0], g_vals[idx1:idx2][-1])
+
+coeffs, res = kak.fit_poly5_from_unwrap(
+    g_vals, yData,
+    order=order,
+    do_plots=True,
+    smooth_yData=False,
+    index=i,
+    wavelength=trim_wave[cut],
+    # unwrap kwargs (tune if needed):
+    prom_frac=prom_frac, dist_frac=dist_frac, savgol_window=savgol_window, 
+    savgol_polyorder=savgol_polyorder
+)
+
+############  '''## Fit 5th-order poly for EACH wavelength and store (with helper) ##''' #############
+order = 5
+Nw = len(trim_wave)
+
+# Coefficient matrix: (Nw, 6) -> [c5, c4, c3, c2, c1, c0]
+coef_mat = np.full((Nw, order + 1), np.nan, dtype=float)
+fit_ok = np.zeros(Nw, dtype=bool)
+
+for i in range(Nw):
+    yData = trim_avg_spectrum[i, idx1:idx2]
+
+    # Unwrap + fit using the helper function
+    coeffs, res = kak.fit_poly5_from_unwrap(
+        g_vals, yData,
+        order=order,
+        do_plots=False,
+        smooth_yData=False,
+        index=i,
+        wavelength=trim_wave[i],
+        # unwrap kwargs (tune if needed):
+        prom_frac=prom_frac, dist_frac=dist_frac, savgol_window=savgol_window, 
+        savgol_polyorder=savgol_polyorder
+    )
+        
+    if coeffs is not None:
+        coef_mat[i, :] = coeffs
+        fit_ok[i] = True
+
+print(f"Fitted {fit_ok.sum()} / {Nw} wavelengths.")
+
+############  '''## Save CSV: wavelength + coefficients ##''' #############
+# out = np.column_stack([trim_wave.reshape(-1, 1), coef_mat])
+# header = "wavelength_nm,c5,c4,c3,c2,c1,c0"
+# np.savetxt("poly5_coeffs_by_wavelength.csv", out, delimiter=",", header=header, comments="")
+# print("Saved coefficients to poly5_coeffs_by_wavelength.csv")
+
+############  '''## Optional visual check on an index (e.g., cut) ##''' #############
+i = cut
+
+if 0 <= i < Nw and fit_ok[i]:
+    c5, c4, c3, c2, c1, c0 = coef_mat[i]
+    poly5 = np.poly1d([c5, c4, c3, c2, c1, c0])
+    phase_fit = poly5(g_vals)
+
+
+    # Recompute unwrapped phase for plotting
+    _, res_test = kak.fit_poly5_from_unwrap(
+        g_vals, trim_avg_spectrum[i, idx1:idx2],
+        order=order, do_plots=False, smooth_yData=False, index=i, 
+        wavelength=trim_wave[i],
+        prom_frac=prom_frac, dist_frac=dist_frac, savgol_window=savgol_window, 
+        savgol_polyorder=savgol_polyorder
+    )
+    
+    phi_test = res_test['phi_unw']
+    #print(phi_test)
+    offset = np.round((phi_test[0] - phase_fit[0]) / (2 * np.pi))
+    print(f"Offset difference: {offset} × 2π")
+
+    if res_test is not None:
+        phi_test = res_test['phi_unw']
+        plt.figure()
+        plt.title(f"λ = {trim_wave[i]:.1f} nm: unwrap + poly5 fit")
+        plt.plot(g_vals, phi_test, 'k.', label='phi_unw')
+        plt.plot(g_vals, phase_fit, '-', label=f'poly{order} fit', color='orange')
+        plt.xlabel('Grayscale Value')
+        plt.ylabel('Phase (rad)')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+############  '''## Plot: polynomial coefficients vs wavelength ##''' #############
+lam_ok = trim_wave[fit_ok]
+coef_ok = coef_mat[fit_ok, :]  # shape: (Nok, 6) -> [c5, c4, c3, c2, c1, c0]
+
+labels = ['c5', 'c4', 'c3', 'c2', 'c1', 'c0']
+for j, lab in enumerate(labels):
+    plt.figure()
+    plt.plot(lam_ok, coef_ok[:, j], lw=1.8)
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel(lab)
+    plt.title(f'{lab} vs wavelength')
+    plt.tight_layout()
+    plt.show()
+
+
