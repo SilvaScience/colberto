bf2886ad05bd2708b78a0637ec4ff5b6fe9fc7c3
Mathieu Desmarais <falvendes@hotmail.com>
2026-07-31 11:22:49 -0400

    Corrections of the cryostat banner and handling.
    - La banderole (arbre de paramètres générique) force maintenant tout
      paramètre du device "cryostat" en lecture seule, peu importe le flag
      read du driver. Le contrôle (setpoint, PID, compresseur) ne passe
      plus que par l'onglet Cryostat dédié (CryostatTab). Les valeurs
      restent visibles dans la banderole et continuent d'alimenter le
      DataHandling/logging.
    
    - Refactor: les deux blocs qui construisaient l'arbre de paramètres
      (constructeur + create_parameter_array) sont fusionnés dans une
      seule méthode _build_parameter_tree_item, pour éviter que les deux
      copies divergent avec le temps.
    
    - CryoDemo.py réécrit pour rester compatible avec CryostatTab:
      - noms de paramètres harmonisés avec CryoPasqal (Set_T, ChannelA_T
        à E_T, PressureF/G, Pid_P/I/D, OnOff_comp, Stability,
        Critical_State) au lieu de set_T/current_T
      - ajout des méthodes set_temperature_setpoint, set_heater_range,
        set_pid_parameters, set_compressor_state, reset_compressor_alarm
        que CryostatTab appelait déjà mais qui n'existaient pas côté demo
        (AttributeError silencieuse au clic sur Apply/compresseur/reset)
      - simulation simple: la température dérive vers le setpoint,
        plus vite si le compresseur est activé
      - correction typo type='Cryostatas' -> 'Cryostat'

--- src/drivers/CryoDemo.py
+++ src/drivers/CryoDemo.py
@@ -3,11 +3,11 @@
 Created on Thu Sep  1 13:26:53 2022
 
 @author: NanoUltrafast2
-Hardware class to controll cryostat. All hardware classes require a definition of
-parameter_dict (set write and read parameter)
-parameter_display_dict (set Spinbox options)
-set_parameter function (assign set functions)
-
+Simulated cryostat driver used when no CryoPasqal hardware is available.
+Mirrors the parameter names (drivers.Optidry250.CryoPasqal.parameter_display_dict)
+and the command methods used by GUI.cryostattab.CryostatTab, so the dedicated
+cryostat page and the generic parameter tree behave the same way in demo mode
+as they do with real hardware.
 """
 
 import numpy as np
@@ -19,94 +19,203 @@ from collections import defaultdict
 class CryoDemo(QtCore.QThread):
 
     name = 'CryoDemo'
-    type = 'Cryostatas'
-    
+    type = 'Cryostat'
+
     def __init__(self):
         super(CryoDemo, self).__init__()
 
-
-        # set parameter dict
-        self.parameter_dict = defaultdict()
-        
-        # setting up variables, open array
-        self.set_T = []
-        self.current_T = []
-        self.stop = False
         self.parameter_display_dict = defaultdict(dict)
-        self.parameter_display_dict['set_T']['val'] = 5
-        self.parameter_display_dict['set_T']['unit'] = ' K'
-        self.parameter_display_dict['set_T']['max'] = 1000
-        self.parameter_display_dict['set_T']['read'] = False
-        self.parameter_display_dict['current_T']['val'] = 5
-        self.parameter_display_dict['current_T']['unit'] = ' K'
-        self.parameter_display_dict['current_T']['max'] = 1000
-        self.parameter_display_dict['current_T']['read'] = True
+
+        # --- Setpoint and Loop settings ---
+        self.parameter_display_dict['Set_T']['val'] = 5.0
+        self.parameter_display_dict['Set_T']['unit'] = ' K'
+        self.parameter_display_dict['Set_T']['min'] = 0
+        self.parameter_display_dict['Set_T']['max'] = 400
+        self.parameter_display_dict['Set_T']['read'] = False
+
+        self.parameter_display_dict['Regulation_Loop']['val'] = 1
+        self.parameter_display_dict['Regulation_Loop']['unit'] = ' loop'
+        self.parameter_display_dict['Regulation_Loop']['min'] = 1
+        self.parameter_display_dict['Regulation_Loop']['max'] = 2
+        self.parameter_display_dict['Regulation_Loop']['read'] = False
+
+        # --- Temperatures (Channels A to E) ---
+        for channel in ['ChannelA_T', 'ChannelB_T', 'ChannelC_T', 'ChannelD_T', 'ChannelE_T']:
+            self.parameter_display_dict[channel]['val'] = 5.0
+            self.parameter_display_dict[channel]['unit'] = ' K'
+            self.parameter_display_dict[channel]['min'] = 0
+            self.parameter_display_dict[channel]['max'] = 400
+            self.parameter_display_dict[channel]['read'] = True
+
+        # --- Pressures (Channels F and G) ---
+        self.parameter_display_dict['PressureF']['val'] = 101300.0
+        self.parameter_display_dict['PressureF']['unit'] = ' Pa'
+        self.parameter_display_dict['PressureF']['min'] = 0
+        self.parameter_display_dict['PressureF']['max'] = 200000
+        self.parameter_display_dict['PressureF']['read'] = True
+
+        self.parameter_display_dict['PressureG']['val'] = 101300.0
+        self.parameter_display_dict['PressureG']['unit'] = ' Pa'
+        self.parameter_display_dict['PressureG']['min'] = 0
+        self.parameter_display_dict['PressureG']['max'] = 200000
+        self.parameter_display_dict['PressureG']['read'] = True
+
+        # --- PID Parameters ---
+        self.parameter_display_dict['Pid_P']['val'] = 0.0
+        self.parameter_display_dict['Pid_P']['unit'] = ' P'
+        self.parameter_display_dict['Pid_P']['min'] = 0
+        self.parameter_display_dict['Pid_P']['max'] = 1000
+        self.parameter_display_dict['Pid_P']['read'] = False
+
+        self.parameter_display_dict['Pid_I']['val'] = 0.0
+        self.parameter_display_dict['Pid_I']['unit'] = ' I'
+        self.parameter_display_dict['Pid_I']['min'] = 0
+        self.parameter_display_dict['Pid_I']['max'] = 1000
+        self.parameter_display_dict['Pid_I']['read'] = False
+
+        self.parameter_display_dict['Pid_D']['val'] = 0.0
+        self.parameter_display_dict['Pid_D']['unit'] = ' D'
+        self.parameter_display_dict['Pid_D']['min'] = 0
+        self.parameter_display_dict['Pid_D']['max'] = 1000
+        self.parameter_display_dict['Pid_D']['read'] = False
+
+        # --- Status and Compressor ---
+        self.parameter_display_dict['OnOff_comp']['val'] = 0
+        self.parameter_display_dict['OnOff_comp']['unit'] = ' state'
+        self.parameter_display_dict['OnOff_comp']['min'] = 0
+        self.parameter_display_dict['OnOff_comp']['max'] = 1
+        self.parameter_display_dict['OnOff_comp']['read'] = False
+
+        self.parameter_display_dict['Stability']['val'] = "In progress"
+        self.parameter_display_dict['Stability']['unit'] = ''
+        self.parameter_display_dict['Stability']['read'] = True
+
+        self.parameter_display_dict['Critical_State']['val'] = "OK"
+        self.parameter_display_dict['Critical_State']['unit'] = ''
+        self.parameter_display_dict['Critical_State']['read'] = True
 
         # set up parameter dict that only contains value. (faster to access)
         self.parameter_dict = {}
         for key in self.parameter_display_dict.keys():
             self.parameter_dict[key] = self.parameter_display_dict[key]['val']
 
+        # --- History for stability calculation ---
+        self.temp_history = []
+        self.stability_threshold = 0.05
+        self.stability_window = 30
 
-        
-        # defining waitTime
-        self.waitTime = 0.1
-
-        # start updating temp
-        self.UpdateWorker = UpdateWorker()
-        self.UpdateWorker.new_T.connect(self.update_temp)
+        # start simulated background polling worker
+        self.UpdateWorker = DemoWorker(self.parameter_dict['ChannelA_T'], self.parameter_dict['Set_T'])
+        self.UpdateWorker.new_T.connect(self.update_all_data)
         self.UpdateWorker.start()
 
-    def set_parameter(self,parameter,value):
-        if parameter == 'set_T':
-            self.update_set_T(value)
+    def set_parameter(self, parameter, value):
+        """Called by the generic parameter tree (banderole) for writable params."""
+        self.parameter_dict[parameter] = value
+        if parameter in self.parameter_display_dict:
+            self.parameter_display_dict[parameter]['val'] = value
+
+        if parameter == 'Set_T':
             self.UpdateWorker.target = value
+        elif parameter == 'OnOff_comp':
+            self.set_compressor_state(bool(int(value)))
 
-    def update_set_T(self, set_temperature):
-        #set temperature to some degree K
-        #requests.put("http://10.131.3.6:47101/v1/controller/properties/platformTargetTemperature",json = {"platformTargetTemperature": set_temperature})
-        
-        print(f'Temperature set to {set_temperature}')
-              
-    def start_cool(self):
-        #start chamber cooldown
-        #requests.post('http://10.131.3.6:47101/v1/controller/methods/cooldown()')
-        
-        print("Cooldown has started")
-        
-    def start_warm(self):
-        # warmup the chamber
-        # requests.post('http://10.131.3.6:47101/v1/controller/methods/warmup()')
-        print("Warming Up")
-        
-    def update_temp(self, new_T):
-        self.parameter_dict['current_T'] = new_T
+    # --- Simulated hardware commands, mirror CryoPasqal's API used by CryostatTab ---
+
+    def set_temperature_setpoint(self, loop, target_temp):
+        self.parameter_dict['Set_T'] = target_temp
+        self.parameter_display_dict['Set_T']['val'] = target_temp
+        self.UpdateWorker.target = target_temp
+        print(f'[Demo] Loop {loop} setpoint updated to: {target_temp} K')
+
+    def set_heater_range(self, loop, range_level):
+        print(f'[Demo] Loop {loop} Heater Range updated to: {range_level}')
+
+    def set_pid_parameters(self, loop, p, i, d):
+        self.parameter_dict['Pid_P'] = p
+        self.parameter_dict['Pid_I'] = i
+        self.parameter_dict['Pid_D'] = d
+        print(f'[Demo] Loop {loop} PID parameters updated: P={p}, I={i}, D={d}')
+
+    def set_compressor_state(self, state_on):
+        self.parameter_dict['OnOff_comp'] = int(bool(state_on))
+        self.parameter_display_dict['OnOff_comp']['val'] = int(bool(state_on))
+        self.UpdateWorker.compressor_on = bool(state_on)
+        print(f"[Demo] Compressor set to: {'ON' if state_on else 'OFF'}")
+
+    def reset_compressor_alarm(self):
+        self.parameter_dict['Critical_State'] = 'OK'
+        self.parameter_display_dict['Critical_State']['val'] = 'OK'
+        print('[Demo] Alarm reset command sent.')
+
+    def update_all_data(self, data_list):
+        """Slot receiving simulated sensor data from the background worker."""
+        def assign_val(key, val):
+            self.parameter_dict[key] = val
+            self.parameter_display_dict[key]['val'] = val
+
+        assign_val('ChannelA_T', data_list[0])
+        assign_val('ChannelB_T', data_list[1])
+        assign_val('ChannelC_T', data_list[2])
+        assign_val('ChannelD_T', data_list[3])
+        assign_val('ChannelE_T', data_list[4])
+        assign_val('PressureF', data_list[5])
+        assign_val('PressureG', data_list[6])
+
+        self.calculate_stability(data_list[0])
+
+    def calculate_stability(self, current_temp):
+        self.temp_history.append(current_temp)
+        if len(self.temp_history) > self.stability_window:
+            self.temp_history.pop(0)
+
+        status = "In progress"
+        if len(self.temp_history) >= self.stability_window:
+            temp_range = max(self.temp_history) - min(self.temp_history)
+            if temp_range <= self.stability_threshold:
+                status = "Stable"
+
+        self.parameter_dict['Stability'] = status
+        self.parameter_display_dict['Stability']['val'] = status
 
 
+class DemoWorker(QtCore.QThread):
+    """Background worker that simulates the cryostat drifting toward its setpoint."""
 
-class UpdateWorker(QtCore.QThread):
+    new_T = QtCore.pyqtSignal(list)
 
-    new_T = QtCore.pyqtSignal(float)
-
-    def __init__(self):
-        super(UpdateWorker, self).__init__()
-        self.currentT = []
+    def __init__(self, initial_temp, initial_target):
+        super(DemoWorker, self).__init__()
         self.stop = False
-        self.waitTime = 0.1
-        self.target = 300
+        self.waitTime = 0.5
+        self.target = initial_target
+        self.current_T = initial_temp
+        self.compressor_on = False
+        self.cooling_rate = 0.5  # K per tick toward target when compressor is on
 
     def run(self):
         while not self.stop:
-            # calling the read temperature function
-            self.readtemp = self.read_T()
-
-            # waiting to remeasure the temperature
+            self.new_T.emit(self.simulate_step())
             time.sleep(self.waitTime)
-            self.new_T.emit(self.readtemp)
 
-    def read_T(self):
-        #read the current platform target temperature
-        #resp=requests.get('http://10.131.3.6:47101/v1/controller/properties/platformTargetTemperature')
-        target = self.target + np.random.rand(1)
-        return target
+    def simulate_step(self):
+        # Drift current_T toward target, faster when the compressor is on
+        rate = self.cooling_rate if self.compressor_on else self.cooling_rate * 0.1
+        delta = self.target - self.current_T
+        step = np.clip(delta, -rate, rate)
+        noise = (np.random.rand() - 0.5) * 0.05
+        self.current_T = self.current_T + step + noise
 
+        pressure_f = 101300.0 + (np.random.rand() - 0.5) * 50
+        pressure_g = 101300.0 + (np.random.rand() - 0.5) * 50
+
+        # Other channels loosely track the primary channel with small fixed offsets
+        return [
+            self.current_T,
+            self.current_T + 0.5,
+            self.current_T + 1.0,
+            self.current_T - 0.5,
+            self.current_T - 1.0,
+            pressure_f,
+            pressure_g,
+        ]
